ba03d3e44a7b926e5956dbe0a42ecf9e175427e8
Clayton Jones <claytonjonesLMP@gmail.com>
2020-04-10 13:38:59 -0700

    categories endpoint tests pass

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -2,90 +2,87 @@
 
 const app = require('../lib/server.js');
 const supergoose = require('@code-fellows/supergoose');
+const categoriesSchema = require('../lib/models/categories-schema.js');
+const productsSchema = require('../lib/models/products-schema.js');
+const Model = require('../lib/models/model.js');
+
+const CategoriesModel = new Model(categoriesSchema);
+const ProductsModel = new Model(productsSchema);
+
+beforeAll(async () => {
+  await CategoriesModel.create({
+    "name": "Test 1"
+  });
+
+  await CategoriesModel.create({
+    "name": "Test 2"
+  });
+
+  await ProductsModel.create({
+    "name": "Product 1",
+    "category": "Category 1"
+  });
+
+  await ProductsModel.create({
+    "name": "Product 2",
+    "category": "Category 2"
+  });
+});
 
 const mockRequest = supergoose(app.server);
 
 describe('categories routes works', () => {
-  it('can get categories', async () => {
-    let response = await mockRequest.get('/categories');
+  it('can get all categories', async () => {
+    let results = await mockRequest.get('/api/v1/categories');
+    expect(results.body.count).toStrictEqual(2);
+    expect(results.status).toStrictEqual(200);
+  });
 
-    expect(response.body).toStrictEqual([
-      {
-        "id": 1,
-        "name": "components"
-      },
-      {
-        "name": "electronics",
-        "id": 2
-      }
-    ]);
+  it('can get one category', async () => {
+    let allCategories = await mockRequest.get('/api/v1/categories');
+    let firstID = allCategories.body.results[0]._id;
+    let oneCategory = await mockRequest.get(`/api/v1/categories/${firstID}`);
+
+    expect(oneCategory.body.name).toStrictEqual('Test 1');
   });
 
   it('can post to categories', async () => {
-    let newCategory = { "name": "hardware" };
+    let results = await mockRequest.post('/api/v1/categories').send({"name": "Test 3"});
 
-    let response = await mockRequest.post('/categories').send(newCategory);
-
-    expect(response.body).toStrictEqual({"name": "hardware", "id": 3});
-    expect(response.status).toStrictEqual(201);
+    expect(results.body.name).toStrictEqual('Test 3');
   });
 
   it('can update a category', async () => {
-    let updatedCategory = { "name": "COMPONENTS" };
-    let response = await mockRequest.put('/categories/1').send(updatedCategory);
+    let allCategories = await mockRequest.get('/api/v1/categories');
+    let firstID = allCategories.body.results[0]._id;
 
-    expect(response.body).toStrictEqual({"id": 1, "name": "COMPONENTS"});
-    expect(response.status).toStrictEqual(200);
+    let results = await mockRequest.put(`/api/v1/categories/${firstID}`).send({"name": "Updated"});
+    expect(results.body.name).toStrictEqual('Updated');
   });
 
   it('can delete from categories', async () => {
-    let response = await mockRequest.delete('/categories/1');
-    expect(response.status).toStrictEqual(200);
+    let allCategories = await mockRequest.get('/api/v1/categories');
+    let firstID = allCategories.body.results[0]._id;
+    let results = await mockRequest.delete(`/api/v1/categories/${firstID}`);
+
+    expect(results.text).toBe(firstID);
   });
 });
 
 describe('products categories routes work', () => {
-  it('can get products', async () => {
-    let response = await mockRequest.get('/products');
-    expect(response.body).toStrictEqual([
-      {
-        "id": 1,
-        "category": "components",
-        "name": "Intel Core i7-9000k CPU"
-      },
-      {
-        "id": 2,
-        "category": "components",
-        "name": "RTX 2080 TI GPU"
-      },
-      {
-        "name": "MOTHERBOARD",
-        "category": "components",
-        "id": 3
-      }
-    ]);
-    expect(response.status).toStrictEqual(200);
+  it('can get all products', async () => {
+
   });
 
   it('can post to products', async () => {
-    let newProduct = { "name": "PRODUCT", "category": "CATEGORY" };
-    let response = await mockRequest.post('/products').send(newProduct);
-
-    expect(response.body).toStrictEqual({ "name": "PRODUCT", "category": "CATEGORY", "id": 4 });
-    expect(response.status).toStrictEqual(201);
 
   });
 
   it('can update products', async () => {
-    let updatedProduct = { "name": "UPDATED", "category": "UPDATED" };
-    let response = await mockRequest.put('/products/1').send(updatedProduct);
 
-    expect(response.body).toStrictEqual({ "name": "UPDATED", "category": "UPDATED", "id": 1 });
-    expect(response.status).toStrictEqual(200);
   });
 
   it('can delete from products', async () => {
-    let response = await mockRequest.delete('/products/1');
-    expect(response.status).toStrictEqual(200);
+
   });
 });
